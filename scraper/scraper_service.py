af4c0f34967a7ee5ae539b93fe08131dffc71f0c
alfonsodurso <84540858+alfonsodurso@users.noreply.github.com>
2025-03-02 19:55:57 +0100

    Update scraper_service.py

--- scraper/scraper_service.py
+++ scraper/scraper_service.py
@@ -1,12 +1,20 @@
 import sys
+import os
+
+# Output di debug per verificare la directory corrente e il PYTHONPATH iniziale
+print("Current working directory:", os.getcwd())
+print("Initial sys.path:", sys.path)
+
+# Aggiungi la directory padre (la root del repository) al PYTHONPATH
+repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, repo_root)
+
+print("Updated sys.path:", sys.path)
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 from db.db_manager import DatabaseManager
 from scraper.parser import AlboParser
 from scraper.telegram_notifier import TelegramNotifier
-
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 
 def job_monitor():
     db_manager = DatabaseManager()
