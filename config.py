7919247710949789a0442475e22467f5d7a61666
alfonsodurso <84540858+alfonsodurso@users.noreply.github.com>
2025-03-02 20:16:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "inserisci_il_tuo_bot_token")
-TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "inserisci_il_tuo_chat_id")
+# Legge i valori dai Secrets di GitHub Actions
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
 BASE_URL = "https://www.halleyweb.com/c065001/mc/"
 ALBO_URL = BASE_URL + "mc_p_ricerca.php?noHeaderFooter=1&multiente=c065001"
