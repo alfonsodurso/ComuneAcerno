05581d6086f8ae4c1e3ae91b63d77b77abea7193
alfonsodurso <84540858+alfonsodurso@users.noreply.github.com>
2025-03-04 20:31:26 +0100

    Update app.py
    Questa modifica imposta la sidebar chiusa di default su mobile:

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,16 +1,16 @@
 import streamlit as st
-
-st.set_page_config(page_title="Albo Pretorio", layout="wide", initial_sidebar_state="expanded")
-
 from common import load_data
 from sfoglia import page_sfoglia
 from elenco import page_elenco
 from analisi import page_analisi
 
+# Sidebar chiusa di default su mobile
+st.set_page_config(page_title="Albo Pretorio", layout="wide", initial_sidebar_state="collapsed")
+
 st.sidebar.title("Navigazione")
 pagina = st.sidebar.radio("Seleziona pagina", ("SFOGLIA", "ELENCO", "ANALISI"))
 
-# Carica i dati dal database
+# Carica i dati
 df = load_data()
 
 # Mostra la pagina selezionata
