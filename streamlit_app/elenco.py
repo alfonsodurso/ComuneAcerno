5e4b21ab9a955b2bf6051a2b886f61683d5f1f46
alfonsodurso <84540858+alfonsodurso@users.noreply.github.com>
2025-03-06 15:54:29 +0100

    Update elenco.py

--- streamlit_app/elenco.py
+++ streamlit_app/elenco.py
@@ -1,16 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-# Impostazione di un numero fisso di righe per pagina
-ROWS_PER_PAGE = 20
-
-# Funzione per paginare i dati
-def paginate_data(df, page):
-    start = page * ROWS_PER_PAGE
-    end = start + ROWS_PER_PAGE
-    return df[start:end]
-
-# Funzione per la visualizzazione della tabella con le specifiche richieste
 def page_elenco(df):
     st.header("📋 ELENCO")
 
@@ -34,20 +24,11 @@ def page_elenco(df):
     if filtered.empty:
         st.info("Nessuna pubblicazione trovata.")
     else:
-        # Numero totale di pagine
-        total_pages = len(filtered) // ROWS_PER_PAGE + (1 if len(filtered) % ROWS_PER_PAGE > 0 else 0)
-
-        # Pagina corrente
-        page = st.slider("Pagina", min_value=0, max_value=total_pages-1, step=1)
-
-        # Pagina dei dati filtrati
-        df_paginated = paginate_data(filtered, page)
-
         # Colonne disponibili
-        available_columns = set(df_paginated.columns)
+        available_columns = set(filtered.columns)
         columns_to_keep = ["numero_pubblicazione", "mittente", "tipo_atto", "data_inizio_pubblicazione", "oggetto_atto", "documento_principale", "allegati"]
         valid_columns = [col for col in columns_to_keep if col in available_columns]
-        df_reduced = df_paginated[valid_columns].copy()
+        df_reduced = filtered[valid_columns].copy()
 
         # Converti NaN in stringhe per colonne con allegati e documenti
         if "documento" in df_reduced:
@@ -63,11 +44,8 @@ def page_elenco(df):
                 return "width: 120px;"
             return ""
 
-        # Mostra la tabella con scorrimento orizzontale e verticale
+        # Mostra la tabella con scorrimento orizzontale
         st.dataframe(
             df_reduced.style.applymap(style_min_width),
             use_container_width=True,
         )
-
-        # Navigazione tra le pagine
-        st.text(f"Pagina {page+1} di {total_pages}")
