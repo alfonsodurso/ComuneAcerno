b7466c5f077dfbbe437faf97a9dca08ce375511a
alfonsodurso <84540858+alfonsodurso@users.noreply.github.com>
2025-03-19 16:37:47 +0100

    Update analisi.py

--- streamlit_app/analisi.py
+++ streamlit_app/analisi.py
@@ -129,7 +129,7 @@ def crea_config_chart(title: str, dataset: pd.DataFrame, selected_cols: list) ->
             "option": {
                 "legend": {
                     "left": "center",
-                    "bottom": "10%",
+                    "bottom": "0%",
                     "orient": "vertical"
                 },
                 "grid": {
