e4d0a2abbd07a2f22da7891f8ba736a5273505d1
alfonsodurso <84540858+alfonsodurso@users.noreply.github.com>
2025-03-19 16:59:16 +0100

    Update analisi.py

--- streamlit_app/analisi.py
+++ streamlit_app/analisi.py
@@ -131,7 +131,8 @@ def crea_config_chart(title: str, dataset: pd.DataFrame, selected_cols: list) ->
                     "orient": "horizontal"
                 },
                 "grid": {
-                    "top": "5%"  # Fa spazio alla legenda
+                    "top": "5%",
+                    "bottom": "5%" # Fa spazio alla legenda
                 }
             }
         }]
