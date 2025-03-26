1739f7fd68ea41629bcef0c54794a3fa55662ba5
alfonsodurso <84540858+alfonsodurso@users.noreply.github.com>
2025-03-26 22:02:05 +0100

    Update analisi.py

--- streamlit_app/analisi.py
+++ streamlit_app/analisi.py
@@ -299,6 +299,8 @@ def display_tipologie_tab(container, df: pd.DataFrame):
         chart_data = prepare_tipologie_count(df)
     
     chart_config = create_doughnut_chart(chart_data)
+    st.write(chart_data)
+
     st_echarts(options=chart_config, height="400px", key="echarts_tipologie")
 
 # -----------------------------------------------------------------
