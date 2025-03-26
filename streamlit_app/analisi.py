572a58dd1ac46b0cc73db91024ba8cab7cc3149b
alfonsodurso <84540858+alfonsodurso@users.noreply.github.com>
2025-03-26 22:09:23 +0100

    Update analisi.py

--- streamlit_app/analisi.py
+++ streamlit_app/analisi.py
@@ -310,10 +310,8 @@ def display_tipologie_tab(container, df: pd.DataFrame):
         chart_data = prepare_tipologie_count(df)
     
     chart_config = create_doughnut_chart(chart_data)
-    if not chart_config:
-        st.warning("I dati per il grafico sono vuoti!")
-    return
     st_echarts(options=chart_config, height="400px", key="echarts_tipologie")
+    st.experimental_rerun()
 
 # -----------------------------------------------------------------
 
